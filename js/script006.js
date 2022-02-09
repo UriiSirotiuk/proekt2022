afbe89a3c86271a61fe3ae57807a88a0ce9fa316
Yura <ZOCHKysnicha@gmail.com>
2022-02-09 07:58:40 +0100

    first commit

---
+++ js/script006.js
@@ -0,0 +1,31 @@
+"use strict";
+
+let  number = 5;
+const leftBolderWidht = 1;
+
+number = 10;
+console.log (number);
+
+console.log(4/0);
+
+console.log('string' * 9);
+
+const persone = "alex"; 
+
+console.log (something);
+
+let unt;
+console.log(unt);
+
+const obj = {
+    name: "JOin",
+    age: 25,
+    isMarrid: false
+
+};
+
+console.log(obj.name);
+
+ let arr= ['plum.png', 'orangrr.jpg','apple.png'];
+ console.log(arr[1]);
+
---
+++ js/script008.js
@@ -0,0 +1,15 @@
+"use strict";
+// alert("hELLO");
+
+// const resuult = confirm("Are you here?");
+// alert(resuult);
+// const ansver = +prompt("вам є 18", "18");
+// alert(typeof(ansver));
+
+const answers =[];
+
+answers[0] = prompt("Яке ваше імя?","");
+answers[1] = prompt("Яке ваша фамилія","");
+answers[2] = prompt("скільки Вам років?","");
+
+document.write(answers);
